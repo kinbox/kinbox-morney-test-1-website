a76f7eff0103643df1825783e37e0ab5dd44a18c
Adin <1502842832@qq.com>
2021-09-10 16:51:31 +0800

    deploy

---
+++ precache-manifest.5c5c6446169eaa37001be6844ada782a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59dc2f5528369ab0909",
+    "url": "/kinbox-morney-test-1-website/css/app.5cb26745.css"
+  },
+  {
+    "revision": "087e944cf009338a559b8d826d2bc9f7",
+    "url": "/kinbox-morney-test-1-website/index.html"
+  },
+  {
+    "revision": "d59dc2f5528369ab0909",
+    "url": "/kinbox-morney-test-1-website/js/app.b35f7c90.js"
+  },
+  {
+    "revision": "6871458316c4e3217180",
+    "url": "/kinbox-morney-test-1-website/js/chunk-vendors.2109d7e6.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/kinbox-morney-test-1-website/manifest.json"
+  },
+  {
+    "revision": "4b0c5d340c4b01b8c9a6861243df26d7",
+    "url": "/kinbox-morney-test-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/kinbox-morney-test-1-website/robots.txt"
+  }
+]);
